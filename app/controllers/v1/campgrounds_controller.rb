76581f2820e29215ce2438391ff5e7f47080446f
Galo O <galfeort@espol.edu.ec>
2025-01-01 18:00:47 -0500

    feat: update campground controller to use URL helper and add strong parameters validation

--- app/controllers/v1/campgrounds_controller.rb
+++ app/controllers/v1/campgrounds_controller.rb
@@ -19,7 +19,7 @@ module V1
       @campground = Campground.new(campground_params)
 
       if @campground.save
-        render :show, status: :created, location: @campground
+        render :show, status: :created, location: v1_campground_url(@campground)
       else
         render json: @campground.errors, status: :unprocessable_entity
       end
@@ -29,7 +29,7 @@ module V1
     # PATCH/PUT /campgrounds/1.json
     def update
       if @campground.update(campground_params)
-        render :show, status: :ok, location: @campground
+        render :show, status: :ok, location: v1_campground_url(@campground)
       else
         render json: @campground.errors, status: :unprocessable_entity
       end
@@ -49,7 +49,7 @@ module V1
 
       # Only allow a list of trusted parameters through.
       def campground_params
-        params.fetch(:campground, {})
+        params.require(:campground).permit(:name, :location, :description, :price)
       end
   end
 end
--- app/models/campground.rb
+++ app/models/campground.rb
@@ -1,2 +1,6 @@
 class Campground < ApplicationRecord
+  validates :name, presence: true
+  validates :location, presence: true
+  validates :description, presence: true
+  validates :price, presence: true
 end
