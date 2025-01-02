df233c94e1331086f2b3d10d446dc1f45db59d10
Galo O <galfeort@espol.edu.ec>
2025-01-02 08:33:24 -0500

    feat: add uniqueness validation for user favorites on campgrounds

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,4 +1,6 @@
 class Favorite < ApplicationRecord
   belongs_to :user
   belongs_to :campground
+
+  validates :user_id, uniqueness: { scope: :campground_id, message: "has already favorited this campground" }
 end
